2e08ce93acaa68d19ff4e3d6536afd3902e3f7ed
sam <samuel.kleinbichain@proton.me>
2024-04-22 17:40:07 +0200

    modification des classe Cowboy et Dame

--- src/western2/Cowboy.java
+++ src/western2/Cowboy.java
@@ -1,14 +1,27 @@
 package western2;
 
+import java.util.ArrayList;
 import western1.Personnage;
 import western1.Boisson;
 
 public class Cowboy extends Personnage{
 
     private int m_argent = 0;
+    private ArrayList<Brigand> brigandCapturer;
 
     public Cowboy(String nom, Boisson boissonFav){
         super(nom,boissonFav);
+        brigandCapturer = new ArrayList<Brigand>();
+    }
+
+    @Override
+    public String sePresenter(){
+        String brigand = "";
+        for(Brigand b : brigandCapturer){
+            brigand += b.getNom()+", ";
+        }
+        brigand = brigand.substring(0,brigand.length()-2);
+        return super.sePresenter() + " J’ai déjà empoché "+m_argent+"$ en capturant "+brigand+".";
     }
 
     public String tirerSur(Brigand brigand){
@@ -17,6 +30,7 @@ public class Cowboy extends Personnage{
 
     public String capturer(Brigand brigand){
         m_argent += brigand.getRecompense();
+        brigandCapturer.add(brigand);
         String brigandString = brigand.estCapturer(this);
         String remercimentDame = "";
         String nomDame = "";
--- src/western2/Dame.java
+++ src/western2/Dame.java
@@ -4,12 +4,28 @@ import western1.Personnage;
 
 public class Dame extends Personnage{
     private boolean m_libre;
+    private Brigand loic; // c'est le méchant de l'histoire
+    private Cowboy sauveur;
 
     public Dame (String surnom) {
         super(surnom);
-        m_libre = false;
+        m_libre = true;
+        loic = null;
+        sauveur = null;
     }
 
+    @Override
+    public String sePresenter(){
+        if(!m_libre){
+            return super.sePresenter()+" Je suis captive de cet escroc de "+loic.getNom()+".";
+        }
+        else if(sauveur!=null){
+            return super.sePresenter()+" Je suis libre grace à "+sauveur.getNom()+".";
+        }
+        else{
+            return super.sePresenter()+" Je suis libre.";
+        }
+    }
     @Override
     public String getNom(){
         return "Miss "+m_nom;
@@ -17,11 +33,13 @@ public class Dame extends Personnage{
 
     String crier(Brigand brigand){
         m_libre = false;
+        loic = brigand;
         return m_nom+" - Hiiii ! Au secours ! "+brigand.getNom()+" m'enlève !";
     }
 
     String remercier(Cowboy cowboy){
         m_libre = true;
+        sauveur = cowboy;
         return m_nom+" - Merci "+cowboy.getNom()+" de m'avoir libérée.";
     }
 
