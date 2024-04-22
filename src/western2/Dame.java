7e8b46c403b3ecb35b7f31517395bad42afa9e7a
Roman <roman.nitzsche.pro@gmail.com>
2024-04-22 20:22:08 +0200

    Modification de brigand pour Western3

--- src/western2/Dame.java
+++ src/western2/Dame.java
@@ -4,10 +4,21 @@ import western1.Personnage;
 
 public class Dame extends Personnage{
     private boolean m_libre;
+    private String m_auteurLiberation;
 
     public Dame (String surnom) {
         super(surnom);
         m_libre = false;
+        m_auteurLiberation = "";
+    }
+
+    public String sePresenter () {
+        String presentation = super.sePresenter();
+        if (m_libre && m_auteurLiberation.length()==0) presentation += " Je suis libre.";
+        else if (m_libre && m_auteurLiberation.length()>0) {
+            presentation += " Je suis libre grâce à mon sauveur " + m_auteurLiberation;
+        }
+        else 
     }
 
     @Override
@@ -22,6 +33,7 @@ public class Dame extends Personnage{
 
     String remercier(Cowboy cowboy){
         m_libre = true;
+        m_auteurLiberation = cowboy.getNom();
         return m_nom+" - Merci "+cowboy.getNom()+" de m'avoir libérée.";
     }
 
