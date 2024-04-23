7392d5c72d0382f79764217807f06bb95e1fb004
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2024-04-23 09:10:54 +0200

    Rajout du contenu de narrateur

--- src/western2/Narrateur.java
+++ src/western2/Narrateur.java
@@ -1,2 +1,15 @@
-package western1.western2;public class Narrateur {
+package western2;
+
+public class Narrateur {
+    String m_lignes = "";
+    public Narrateur () {
+    }
+
+    public void ajouterLignes(String lignes) {
+        m_lignes += lignes+"\n";
+    }
+
+    public void raconterHistoire() {
+        System.out.println(m_lignes);
+    }
 }
